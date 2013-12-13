ec8b4ae967206ce2bcc9515156a4e297312850a4
deathek <deathek@gmail.com>
2013-12-13 23:16:51 +0100

    Extended game Category enum

--- build/generated-sources/jax-ws/hangman/client/Category.java
+++ build/generated-sources/jax-ws/hangman/client/Category.java
@@ -1,6 +1,7 @@
 
 package hangman.client;
 
+import hangman.client.view.SpecialEnum;
 import javax.xml.bind.annotation.XmlEnum;
 import javax.xml.bind.annotation.XmlType;
 
@@ -39,8 +40,7 @@ import javax.xml.bind.annotation.XmlType;
  */
 @XmlType(name = "category")
 @XmlEnum
-public enum Category {
-
+public enum Category implements SpecialEnum {
     BABY_ROOM,
     BATHROOM,
     BEDROOM,
@@ -60,6 +60,52 @@ public enum Category {
     UNIVERSE_SPACE,
     WILD_ANIMALS_PETS,
     WORKSHOP_TOOLS;
+
+
+    @Override
+    public String toString() {
+        switch (this) {
+            case BABY_ROOM:
+                return "Pokój dziecięcy";
+            case BATHROOM:
+                return "Łazienka";
+            case BEDROOM:
+                return "Sypialnia";
+            case CAR_PARTS:
+                return "Części samochodowe";
+            case CLOTHES:
+                return "Ubrania";
+            case CONSTRUCTION_SITE:
+                return "Budowa";
+            case COOKING_INSTRUCTIONS:
+                return "Przepisy kucharskie";
+            case COUNTRIES:
+                return "Kraje";
+            case DINING_ROOM:
+                return "Jadalnia";
+            case FAMILY_MEMBERS:
+                return "Rodzina";
+            case FRUIT_VEGETABLES:
+                return "Owoce, warzywa";
+            case JOBS:
+                return "Praca";
+            case KITCHEN:
+                return "Kuchnia";
+            case LIVING_ROOM:
+                return "Salon";
+            case OFFICE_EQUIPMENT:
+                return "Wyposażenie biurowe";
+            case PARTS_OF_THE_BODY:
+                return "Części ciała";
+            case UNIVERSE_SPACE:
+                return "Przestrzeń kosmiczna";
+            case WILD_ANIMALS_PETS:
+                return "Zwierzęta";
+            case WORKSHOP_TOOLS:
+                return "Narzędzia";
+        }
+        return "";
+    }
 
     public String value() {
         return name();
